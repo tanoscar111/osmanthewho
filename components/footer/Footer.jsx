e42f71ee84ab82672e48ad3c65b350ddf898dcca
ynsmrsk <ynsmrsk13@gmail.com>
2020-12-09 04:47:15 +0300

    Create Footer component

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -2,39 +2,36 @@ import Link from 'next/link'
 
 export default function Footer() {
 	return (
-		<footer class='footer'>
+		<footer className='footer'>
+
+			<div className='footer-headline'>
+				<h2>LET'S MAKE</h2>
+				<h1>GREAT THINGS TOGETHER.</h1>
+				<button className='button-cta'>
+					<h2>HIT ME UP ➤</h2>
+				</button>
+			</div>
 
 			<Link href='#'>
-				<a class='footer-logo'>
-					<img src="../../icons/osman/osman-white.svg" alt="osmanthewho" />
+				<a className='footer-logo'>
+					<img src="../../images/osman/osmanthewho.png" alt="osmanthewho" />
 				</a>
 			</Link>
 
-			<div className="footer-social">
+			<p className='footer-punchline'>Enough about me, tell me about you.</p>
+
+			<div className='footer-social'>
 					<a href='#'><img src="../../icons/social/instagram.svg" alt="Instagram" /></a>
 					<a href='#'><img src="../../icons/social/youtube.svg" alt="YouTube" /></a>
 					<a href='#'><img src="../../icons/social/vimeo.svg" alt="Vimeo" /></a>
 					<a href='#'><img src="../../icons/social/behance.svg" alt="Behance" /></a>
 			</div>
 
-			<div className="footer-links col1">
-				<Link href="#"><a>About Me</a></Link>
-				<Link href="#"><a>Get Contact</a></Link>
-				<Link href="#"><a>My Works</a></Link>
-			</div>
+			<div className='footer-email'><Link href="#"><a>info@osmanthewho.com</a></Link></div>
 
-			<div className="footer-links col2">
-				<Link href="#"><a>info@osmanthewho.com</a></Link>
-				<Link href="#"><a>(+90) 545 600 9986</a></Link>
-				<Link href="#"><a>amk ne yazsam buraya</a></Link>
-			</div>
+			<span className='blank-span'></span>
 
-			<div className="footer-cta">
-				<Link href="#"><a class='button-cta'>Get in touch</a></Link>
-				<div className="footer-copyright">
-					© Osmanthewho 2020 All Rights Reserved.
-				</div>
-			</div>
+			<p className='footer-copyright'>©Made with ♥ by Yunus Emre.</p>
 
 		</footer>
 	)
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Layout from '../components/constants/Layout'
+import SmoothScroll from '../components/constants/SmoothScroll'
 
 export default function Home() {
   return (
-		<Layout>
-		</Layout>
+		<SmoothScroll>
+			<Layout>
+			</Layout>
+		</SmoothScroll>
   )
 }
