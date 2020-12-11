981e3c98bc7d76d2ca821e63a37ae36f12b5bec1
ynsmrsk <ynsmrsk13@gmail.com>
2020-12-11 19:53:23 +0300

    Fix form action adress error.

--- components/contactForm/ContactForm.jsx
+++ components/contactForm/ContactForm.jsx
@@ -8,7 +8,7 @@ export default function ContactForm() {
 
 	return (
 		<div className='form-container'>
-			<form className="kwes-form" action="https://kwes.io/api/foreign/forms/bb6T6UBiO4UuyhmOOkHu">
+			<form className="kwes-form" action="https://kwes.io/api/foreign/forms/JHCSMM3SCuGJdAXe5w09">
 				<label>Your name<input name="name" type="text" placeholder="Fenasi Kerim" /></label>
 				<label>Your email<input name="email" type="email" required placeholder="Ar$1zBe!A@hotmail.com" /></label>
 				<label>Your message<textarea name="message" rows="5" placeholder="guzel site is yapar👌" /></label>
