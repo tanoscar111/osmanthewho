aaf9b20bf770f9c1dcc135a468e814fdfd754c4c
ynsmrsk <ynsmrsk13@gmail.com>
2020-12-19 17:25:17 +0300

    Fuck build erros again.

--- components/contactForm/ContactForm.jsx
+++ components/contactForm/ContactForm.jsx
@@ -1,14 +1,14 @@
 import { useEffect } from 'react'
-import kwesforms from 'kwesforms';
+//import kwesforms from 'kwesforms';
 
-/*export default function ContactForm() {
-	useEffect(() => {
+export default function ContactForm() {
+	/*useEffect(() => {
 		kwesforms.init();
 	}, [])*/
-
+//action="https://kwes.io/api/foreign/forms/JHCSMM3SCuGJdAXe5w09"
 	return (
 		<div className='form-container'>
-			<form className="kwes-form" action="https://kwes.io/api/foreign/forms/JHCSMM3SCuGJdAXe5w09">
+			<form className="kwes-form" action="mailto:ynsmrsk13@gmail.com" method="post">
 				<label>Your name<input name="name" type="text" /></label>
 				<label>Your email<input name="email" type="email" required /></label>
 				<label>Your message<textarea name="message" rows="5" /></label>
