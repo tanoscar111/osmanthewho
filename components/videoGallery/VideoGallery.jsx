7976df82918f6c6109317512399b32e8e9cbcf9e
ynsmrsk <ynsmrsk13@gmail.com>
2020-12-30 06:53:10 +0300

    Create home page's landing section.

--- components/videoGallery/VideoGallery.jsx
+++ components/videoGallery/VideoGallery.jsx
@@ -12,7 +12,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-270'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -22,7 +22,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-270'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/4FpwWe3jay0I5WYdJf/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/4FpwWe3jay0I5WYdJf/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -32,7 +32,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-270'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/PoV2crzAgRoFZhR5rv/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/PoV2crzAgRoFZhR5rv/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -42,7 +42,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-270'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/BTspWq5jRd2QTHwICy/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/BTspWq5jRd2QTHwICy/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -52,7 +52,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-440-550'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/kFBglFnPtvlQn5k3uB/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/kFBglFnPtvlQn5k3uB/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -62,7 +62,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-440-550'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/LnaLrBbOwSkpfT2Yjx/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/LnaLrBbOwSkpfT2Yjx/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -72,7 +72,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-270'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/eJc6nK5xoHD2jMC8mP/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/eJc6nK5xoHD2jMC8mP/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -82,7 +82,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-270'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/YLtsUfroNDjno8q1MH/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/YLtsUfroNDjno8q1MH/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -92,7 +92,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-270'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/5rvbjla0ic8y85KAZV/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/5rvbjla0ic8y85KAZV/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -102,7 +102,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-270'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/vlAFEr3HFGf9nsW43S/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/vlAFEr3HFGf9nsW43S/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -112,7 +112,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-440-550'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/r8yEoWu6JSp6W5R438/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/r8yEoWu6JSp6W5R438/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -122,7 +122,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-440-550'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/QYtmJAkR8STjJ6kmeM/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/QYtmJAkR8STjJ6kmeM/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -134,7 +134,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-209'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/7YihPFNRH0GU9NGaRt/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/7YihPFNRH0GU9NGaRt/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -144,7 +144,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-480-270'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/ovN710as1hM9lmE4fA/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/ovN710as1hM9lmE4fA/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
@@ -154,7 +154,7 @@ export default function VideoGallery() {
 					<Link href="/work">
 						<a>
 							<div className='video-thumbnail px-384-480'>
-								<video autoPlay="autoplay" loop src="https://media.giphy.com/media/7Sw0vVD2Nj2MXNO2cB/giphy.mp4" />
+								<video autoPlay loop src="https://media.giphy.com/media/7Sw0vVD2Nj2MXNO2cB/giphy.mp4" />
 							</div>
 						</a>
 					</Link>
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,15 @@ export default function Home() {
   return (
 		<Layout>
 			<SmoothScroll>
-				<div className='landing'>
-					<h1>cool shit coming soon.</h1>
+				<div className='landing full-bleed'>
+					<video muted playsInline autoPlay loop className="video-full-screen" src="/showreel.m4v" />
+					<div className='landing-content'>
+						<h1 className="landing-content-title">BE WILD HAVE MOVE</h1>
+						<p className="landing-content-subtitle">Kurumus bogazim apple adobe gotten skm</p>
+					</div>
+				</div>
+				<div className="skills">
+					<h1>skills goes here.</h1>
 				</div>
 			</SmoothScroll>
 		</Layout>
