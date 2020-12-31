7d08f34c2e1a1755e5592bbdff8355852c82fbc1
ynsmrsk <ynsmrsk13@gmail.com>
2020-12-31 07:15:08 +0300

    Add scroll animations to the index page.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Layout from '../components/constants/Layout'
 import SmoothScroll from '../components/constants/SmoothScroll'
+import { motion } from 'framer-motion'
 
 export default function Home() {
+	useEffect(() => {
+		const h1 = document.getElementById("h1")
+		console.log(h1.style)
+		document.addEventListener('scroll', (e) => {
+			let scrolled = document.documentElement.scrollTop / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+			h1.style.setProperty('--percentage', `${scrolled * 100}%`)
+		})
+		let video = document.querySelector('video')
+		window.addEventListener('scroll', function() {
+			let value = 0.4 + window.scrollY / -1300
+			video.style.opacity = value
+		})
+	}, [])
+
   return (
 		<Layout>
 			<SmoothScroll>
 				<div className='landing full-bleed'>
-					<video muted playsInline autoPlay loop className="video-full-screen" src="/showreel.m4v" />
+					<video preload="true" muted autoPlay loop className="video-full-screen" src="/showreel.m4v" />
 					<div className='landing-content'>
 						<h1 className="landing-content-title">BE WILD HAVE MOVE</h1>
 						<p className="landing-content-subtitle">Kurumus bogazim apple adobe gotten skm</p>
 					</div>
 				</div>
 				<div className="skills">
-					<h1>skills goes here.</h1>
+					<h1 id="h1">LET ME SHOW YOU WHAT I DO</h1>
 				</div>
 			</SmoothScroll>
 		</Layout>
