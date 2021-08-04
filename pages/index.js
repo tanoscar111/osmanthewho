d0d1526760bf53daa4e39f09a13f8a07ad1d7a98
Yunus Emre <yunusemre@pop-os.localdomain>
2021-08-04 19:54:44 +0300

    Small typography changes..

--- pages/index.js
+++ pages/index.js
@@ -44,8 +44,8 @@ export default function Home() {
 			<section className="self-intro">
 				<TextReveal>
 					<p>
-						I promote living your life to the fullest <br />
-						and seeing the world through an optimistic lens.
+						I promote living your life to the fullest and <br />
+						seeing the world through an optimistic lens.
 					</p>
 					<p>
 						I'm Osman. I'm a filmmaker, photographer, <br />
@@ -53,7 +53,7 @@ export default function Home() {
 					</p>
 					<p>
 						I produce creative contents for brands. <br />
-						Hit me up!
+						Hit me up to collab!
 					</p>
 				</TextReveal>
 			</section>
@@ -70,7 +70,7 @@ export default function Home() {
 					<div id="img-wrapper_1-1" className="image-wrapper" data-speed="0.2"><ParallaxOverflow><img src="/images/skills/1-1.jpg"/></ParallaxOverflow></div>
 					<div id="img-wrapper_1-2" className="image-wrapper" data-speed="0.2"><ParallaxOverflow><img src="/images/skills/1-2.jpg"/></ParallaxOverflow></div>
 					<div id="img-wrapper_1-3" className="image-wrapper" data-speed="0.2"><ParallaxOverflow><img src="/images/skills/1-3.jpg"/></ParallaxOverflow></div>
-					<div id="img-wrapper_2-1" className="image-wrapper" data-speed="0.6"><ParallaxOverflow><img src="/images/skills/2-1.jpg"/></ParallaxOverflow></div>
+					<div id="img-wrapper_2-1" className="image-wrapper" data-speed="0.7"><ParallaxOverflow><img src="/images/skills/2-1.jpg"/></ParallaxOverflow></div>
 					<div id="img-wrapper_2-2" className="image-wrapper" data-speed="0.6"><ParallaxOverflow><img src="/images/skills/2-2.jpg"/></ParallaxOverflow></div>
 					<div id="img-wrapper_2-3" className="image-wrapper" data-speed="0.6"><ParallaxOverflow><img src="/images/skills/2-3.jpg"/></ParallaxOverflow></div>
 					<div id="img-wrapper_3-1" className="image-wrapper" data-speed="0.8"><ParallaxOverflow><img src="/images/skills/3-1.jpg"/></ParallaxOverflow></div>
