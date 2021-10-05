6ef491c4f5155a4e4af9f33793f0ab457f56798c
Yunus Emre <yunusemre@pop-os.localdomain>
2021-10-06 00:30:54 +0300

    Fix video thumnails on works page

--- components/videoGallery/VideoGallery.jsx
+++ components/videoGallery/VideoGallery.jsx
@@ -5,209 +5,185 @@ export default function VideoGallery() {
 	return (
 		<div className="video-gallery-container-container full-bleed">
 			<div className="video-gallery-container">
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-480-270">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-480-270">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/2019-showreel.jpg"
+								src="https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-480-270">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/4FpwWe3jay0I5WYdJf/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-480-270">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/istanbul.jpg"
+								src="https://media.giphy.com/media/4FpwWe3jay0I5WYdJf/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-480-270">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/PoV2crzAgRoFZhR5rv/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-480-270">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/rapid.jpg"
+								src="https://media.giphy.com/media/PoV2crzAgRoFZhR5rv/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-480-270">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/BTspWq5jRd2QTHwICy/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-480-270">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/lunar.jpg"
+								src="https://media.giphy.com/media/BTspWq5jRd2QTHwICy/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-440-550">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/kFBglFnPtvlQn5k3uB/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-440-550">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/whereami.jpg"
+								src="https://media.giphy.com/media/kFBglFnPtvlQn5k3uB/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-440-550">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/LnaLrBbOwSkpfT2Yjx/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-440-550">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/kickit.jpg"
+								src="https://media.giphy.com/media/LnaLrBbOwSkpfT2Yjx/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-480-270">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/eJc6nK5xoHD2jMC8mP/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-480-270">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/avci.jpg"
+								src="https://media.giphy.com/media/eJc6nK5xoHD2jMC8mP/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-480-270">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/YLtsUfroNDjno8q1MH/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-480-270">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/showreel.jpg"
+								src="https://media.giphy.com/media/YLtsUfroNDjno8q1MH/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-480-270">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/5rvbjla0ic8y85KAZV/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-480-270">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/spx.jpg"
+								src="https://media.giphy.com/media/5rvbjla0ic8y85KAZV/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-480-270">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/vlAFEr3HFGf9nsW43S/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-480-270">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/northmen.jpg"
+								src="https://media.giphy.com/media/vlAFEr3HFGf9nsW43S/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-440-550">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/r8yEoWu6JSp6W5R438/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-440-550">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/gymfactory.jpg"
+								src="https://media.giphy.com/media/r8yEoWu6JSp6W5R438/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
-				<div className="video">
-					<Link href="/work">
-						<a>
-							<div className="px-440-550">
-								<video
-									playsInline
-									autoPlay
-									loop
-									muted
-									poster="/images/showreel.png"
-									src="https://media.giphy.com/media/QYtmJAkR8STjJ6kmeM/giphy.mp4"
-								/>
-							</div>
-						</a>
-					</Link>
-				</div>
+				<Link href="/work">
+					<a>
+						<div className="px-440-550">
+							<video
+								playsInline
+								autoPlay
+								loop
+								muted
+								poster="/images/video-thumbnails/tennis.jpg"
+								src="https://media.giphy.com/media/QYtmJAkR8STjJ6kmeM/giphy.mp4"
+							/>
+						</div>
+					</a>
+				</Link>
 
 				{/*
 
