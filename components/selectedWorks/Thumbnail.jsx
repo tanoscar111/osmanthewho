58cba34ee47cc0238fafed6758e2dead33425289
Yunus Emre Işık <yunusemre@192.168.1.43>
2022-03-02 05:07:31 +0300

    Create selected works' data, reusable Thumbnail component and its style.

---
+++ components/selectedWorks/Thumbnail.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState, useRef } from 'react'
+
+export default function Thumbnail({ randWidth, randHeight, randX, randY, title, img, url }) {
+	const video = useRef(null)
+	const [focus, setFocus] = useState(false)
+
+	const handleMouseOver = () => setFocus(true)
+	const handleMouseOut = () => setFocus(false)
+
+	useEffect(() => {
+		if (focus == true) {
+			video.current.play()
+		} else {
+			video.current.pause()
+			video.current.load()
+		}
+	}, [focus])
+
+	const styles = {
+		width: `${randWidth}vw`,
+		height: `${randHeight}vw`,
+		left: `${randX}px`,
+		top: `${randY}px`,
+	}
+
+	return (
+		<div
+		 className="thumbnail"
+		 style={styles}
+		 onMouseOver={handleMouseOver}
+		 onMouseOut={handleMouseOut}
+		>
+			<span className="thumbnail-title">{title}</span>
+			<video
+				className="thumbnail-video"
+				preload="none"
+				playsInline
+				muted
+				loop
+				ref={video}
+				poster={img}
+				src={url}
+			/>
+		</div>
+	)
+}
---
+++ components/selectedWorks/selected-works-data.js
@@ -0,0 +1,111 @@
+export const data = [
+	{
+		id: 1,
+		title: "Avcı Interior",
+		img: "/images/productions/interior.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 2,
+		title: "The Passion",
+		img: "/images/productions/the-passion.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 3,
+		title: "#artwithus | agt",
+		img: "/images/productions/art-with-us.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 4,
+		title: "Showreel",
+		img: "/images/productions/showreel.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 5,
+		title: "Lunar Circus Promo Film",
+		img: "/images/productions/lunar.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 6,
+		title: "SPX - How Big",
+		img: "/images/productions/spx.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 7,
+		title: "TANDEM DANCE THEATRE PROMO FILM",
+		img: "/images/productions/tandem-promo.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 8,
+		title: "NORTHMEN 1. YIL FILM",
+		img: "/images/productions/northmen.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 9,
+		title: "TANDEM DANCE THEATRE TEASER",
+		img: "/images/productions/tandem-promo-teaser.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 10,
+		title: "FINAL JAM RECAP",
+		img: "/images/productions/final-jam-recap.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 11,
+		title: "KRAFT FESTIVAL",
+		img: "/images/productions/kraft-festival.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 12,
+		title: "RAPID DANCE",
+		img: "/images/productions/rapid-dance.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 13,
+		title: "SPA SOUL | HYATT CENTRİC LEVENT İSTANBUL",
+		img: "/images/productions/spa-soul.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 14,
+		title: "RENEKA R80 COMMERCIAL",
+		img: "/images/productions/reneka.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 15,
+		title: "SHOWREEL",
+		img: "/images/productions/2019-reel.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 16,
+		title: "CRYSTAL GELATO FILM",
+		img: "/images/productions/crystal-gelato.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 17,
+		title: "THE SUDD COFFEE – CHRISTMAS FILM",
+		img: "/images/productions/christmas.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+	},
+	{
+		id: 18,
+		title: "CRYSTAL & SMACH EVENT FIL",
+		img: "/images/productions/crystal-event.jpg",
+		url: "https://media.giphy.com/media/q0PfxxVSqk0RTyThEU/giphy.mp4",
+		// youtube
+	},
+]
